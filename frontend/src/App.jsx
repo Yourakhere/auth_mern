9a13b6d5183df6c84efad6bc2819be3e5d95a581
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-02-24 15:39:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,26 @@ import { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import 'react-toastify/dist/ReactToastify.css'
+import Login from './pages/Login'; // Corrected import statement
+import Signup from './pages/Signup'; // Corrected import statement
+import 'react-toastify/dist/ReactToastify.css';
 import RefreshHandler from './pages/RefreshHandler';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to='/login' />
-  }
+    return isAuthenticated ? element : <Navigate to='/login' />;
+  };
 
   return (
     <>
       <div className="App">
-        <RefreshHandler  setIsAuthenticated={setIsAuthenticated}/>
+        <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
         <Routes>
-          <Route path="/home" element={<PrivateRoute element={<Home/>} />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
           <Route path="/" element={<Navigate to='/login' />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </>
