650c1387ba691e51c2c320ea568d9bd2c96cab81
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-02-24 12:17:51 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');  
-const ProductsRouter = require('./Routes/ProductsRouter');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+require("dotenv").config();
+require("./Models/db");
+const PORT = process.env.PORT || 5654;
 
-require('dotenv').config();
-require('./Models/db');
-
-app.get('/ak', (req, res) => {
-    res.send('Abhishek Rock');
+app.get("/ak", (req, res) => {
+  res.send("Abhishek Rock");
 });
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/auth', AuthRouter); 
-app.use('/products', ProductsRouter); 
+app.use("/auth", AuthRouter);
 
-// Export the app as a serverless function for Vercel
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
