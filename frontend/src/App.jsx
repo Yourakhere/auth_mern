82c7e8667235db1c8c084691f7efa56b018bead9
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-03-12 13:12:04 +0530

    Update App.jsx refresh in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';  
 import Signup from './pages/Signup'; 
 import 'react-toastify/dist/ReactToastify.css';
-import RefreshHandler from './pages/RefreshHandler';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem('isAuthenticated') === 'true'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated);
+  }, [isAuthenticated]);
+
   const PrivateRoute = ({ element }) => {
     return isAuthenticated ? element : <Navigate to='/login' />;
   };
@@ -16,11 +22,10 @@ function App() {
   return (
     <>
       <div className="App">
-        <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
         <Routes>
           <Route path="/home" element={<PrivateRoute element={<Home />} />} />
-          <Route path="/" element={<Navigate to='/login' />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Navigate to='/login' />} />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
